53645c967cb724d3654cb5ea26cde20bda627aa1
Jouni Roivas <jroivas@iki.fi>
2019-04-02 19:42:54 +0300

    Fetch catch2 header

--- test.sh
+++ test.sh
@@ -4,6 +4,8 @@ CXX=${CXX:-g++}
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 BDIR="${DIR}/build"
 mkdir -p "${BDIR}"
+mkdir -p "${DIR}/catch2"
+curl -s "https://raw.githubusercontent.com/catchorg/Catch2/master/single_include/catch2/catch.hpp" -o "${DIR}/catch2/catch.hpp"
 
 time $CXX -I"${DIR}/include" "${DIR}/test/test1.cpp" -o "${BDIR}"/test1-catch-me2
 time $CXX -I"${DIR}/include" "${DIR}/test/test2a.cpp" "${DIR}/test/test2b.cpp" "${DIR}/test/test2.cpp" -o "${BDIR}"/test2-catch-me2
